8c47f688e4df705e252ed8c57fc52020c5f05b6a
Mike Korobov <kmike84@gmail.com>
2009-12-05 06:02:43 +0500

    Почистил __init__.py от своих экспериментом, не место им в системе контроля версий

--- pymorphy/__init__.py
+++ pymorphy/__init__.py
@@ -1,12 +1,2 @@
 #coding: utf8
-
 from pymorphy.morph import get_pickle_morph, get_shelve_morph, setup_psyco
-
-if __name__ == '__main__':
-    m = get_shelve_morph('ru')
-#    m.get_graminfo(u"МОСКВА")
-    for form in m.get_graminfo(u'НАИНЕВЕРОЯТНЕЙШИЙ'):
-        for v in form:
-            print v, form[v]
-        print '----'
-
