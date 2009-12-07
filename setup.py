8e7d6928c3861566fe5853012055ae687a455cc9
Mike Korobov <kmike84@gmail.com>
2009-12-07 13:05:12 +0500

    Ссылка на документацию

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme = open(os.path.join(os.path.dirname(__file__), 'README')).read()
 
 setup(
     name     = 'pymorphy',
-    version  = '0.1',
+    version  = '0.1.1',
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
     url='http://bitbucket.org/kmike/pymorphy/',
