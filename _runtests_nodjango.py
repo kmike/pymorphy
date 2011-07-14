da0e6417c58afee7477f20fcc4259c679751fa20
Mike Korobov <kmike84@gmail.com>
2011-07-15 01:17:40 +0600

    переименовал файл с тестами для contrib.tokenizers
    --HG--
    rename : pymorphy/morph_tests/split.py => pymorphy/morph_tests/tokenizers.py

--- _runtests_nodjango.py
+++ _runtests_nodjango.py
@@ -4,7 +4,7 @@ from pymorphy.morph_tests.utilities import *
 from pymorphy.morph_tests.base import *
 from pymorphy.morph_tests.dirty import *
 from pymorphy.morph_tests.hyphen import *
-from pymorphy.morph_tests.split import *
+from pymorphy.morph_tests.tokenizers import *
 from pymorphy.morph_tests.thread_bugs import *
 
 if __name__ == '__main__':
